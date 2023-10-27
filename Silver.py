adc1b6cad1ea505785ca98ff24fb2f9adcee5eac
CasparPetchQualyfi <144714431+CasparPetchQualyfi@users.noreply.github.com>
2023-10-27 15:35:18 +0100

    Update Silver.py

--- Silver.py
+++ Silver.py
@@ -26,8 +26,7 @@ schema = StructType([
 ]) 
 
 
-spark.conf.set(
-"fs.azure.account.key.datacohortworkspacelabs.blob.core.windows.net", "NUwhjFcHG95EU1Rnu7+Woq3JQP28bXy5kDQhA9yFV68XBz1umr7uqeQgMhrwxHfTwNWxAx/n1K6j+AStGTUMkQ==")
+spark.conf.set("fs.azure.account.key.datacohortworkspacelabs.dfs.core.windows.net", "NUwhjFcHG95EU1Rnu7+Woq3JQP28bXy5kDQhA9yFV68XBz1umr7uqeQgMhrwxHfTwNWxAx/n1K6j+AStGTUMkQ==")
 
 # Applying custom schema to data frame 
 #df = spark.read.csv("wasbs://bronze-cp@datacohortworkspacelabs.blob.core.windows.net/yellow_tripdata", header=True, schema=schema)
